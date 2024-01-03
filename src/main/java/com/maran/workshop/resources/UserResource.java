cd51c029b418e4ffdbc92325fa15f907844a34fa
Ari Maran <aricmaran@gmail.com>
2024-01-03 10:03:12 -0300

    User entity and resource

---
+++ src/main/java/com/maran/workshop/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.maran.workshop.resources;
+
+import com.maran.workshop.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user = new User(1, "Maria", "maria@email.com", "999999", "1234");
+        return ResponseEntity.ok().body(user);
+    }
+
+}
