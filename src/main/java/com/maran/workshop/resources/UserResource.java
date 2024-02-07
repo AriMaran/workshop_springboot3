1f71bdb756a6c529f20662d5aa31d6cfd19a3523
Ari Maran <aricmaran@gmail.com>
2024-02-07 13:00:22 -0300

    Insert and Delete methods created

--- src/main/java/com/maran/workshop/resources/UserResource.java
+++ src/main/java/com/maran/workshop/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.maran.workshop.entities.User;
 import com.maran.workshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +27,20 @@ public class UserResource {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj); //ResponseEntity.ok to indicate that was everything correct
     }
+    @PostMapping //While @GetMapping is used for retrieving data, @PostMapping is used for inserting data into the database.
+    public ResponseEntity<User> insert(@RequestBody User obj){
 
+        /*To deserialize a JSON request body into a User object, you can use the @RequestBody annotation.
+        This annotation tells Spring to convert the HTTP request body into an object of the specified class.
+        */
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj); //The create method expects a URI format and the response is expected to contain a
+                                                      // header with the new address of the inserted resource.
+    }
+    @DeleteMapping(value ="/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build(); //return empty response. HTTP 204
+    }
 }
